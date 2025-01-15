530b5f9b698e8af501782502fdb5901a9debe0b8
nathy-melo <natymelo2008@gmail.com>
2025-01-15 12:07:42 -0300

    Add files via upload

--- jogo.js
+++ jogo.js
@@ -5,16 +5,29 @@ sprites.src = "./sprites.png";
 const canvas = document.querySelector("#game-canvas");
 const contexto = canvas.getContext("2d");
 
+const flappyBird = {
+    spriteX: 0,
+    spriteY: 0,
+    largura: 35,
+    altura: 25,
+    x: 10,
+    y: 50,
+        desenha(){
+            contexto.drawImage(
+                sprites,
+                flappyBird.spriteX, flappyBird.spriteY,
+                flappyBird.largura, flappyBird.altura,
+                flappyBird.x, flappyBird.y,
+                flappyBird.largura, flappyBird.altura,
+            );
+
+        }
+};
 
 function loop(){
-    contexto.drawImage(
-        sprites,
-        0, 0,
-        35, 25,
-        10, 50,
-        35, 25)
+    flappyBird.desenha();
 
-        requestAnimationFrame(loop);
-}
+    requestAnimationFrame(loop);
+};
 
 loop();
