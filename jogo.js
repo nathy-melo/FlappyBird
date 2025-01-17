231b9e9422af55c500eeb0c626de73dda765f740
Nathally <natymelo2008@gmal.com>
2025-01-17 10:21:17 -0300

    parte 1

--- jogo.js
+++ jogo.js
@@ -5,6 +5,9 @@ sprites.src = "./sprites.png";
 const canvas = document.querySelector("#game-canvas");
 const contexto = canvas.getContext("2d");
 
+contexto.fillStyle = "#70c5ce";
+contexto.fillRect(0, 0, canvas.width, canvas.height);
+
 const flappyBird = {
     spriteX: 0,
     spriteY: 0,
@@ -24,7 +27,64 @@ const flappyBird = {
         }
 };
 
+
+const cidade = {
+    spriteX: 390,
+    spriteY: 0,
+    largura: 274,
+    altura: 202,
+    x: 0,
+    y: 280,
+        desenha(){
+            contexto.drawImage(
+                sprites,
+                cidade.spriteX, cidade.spriteY,
+                cidade.largura, cidade.altura,
+                cidade.x, cidade.y,
+                cidade.largura, cidade.altura,
+            );
+            contexto.drawImage(
+                sprites,
+                cidade.spriteX, cidade.spriteY,
+                cidade.largura, cidade.altura,
+                cidade.x+cidade.largura, cidade.y,
+                cidade.largura, cidade.altura,
+            );
+
+        }
+};
+
+const chao = {
+    spriteX: 0,
+    spriteY: 610,
+    largura: 224,
+    altura: 112,
+    x: 0,
+    y: 368,
+        desenha(){
+            contexto.drawImage(
+                sprites,
+                chao.spriteX, chao.spriteY,
+                chao.largura, chao.altura,
+                chao.x, chao.y,
+                chao.largura, chao.altura,
+            );
+            contexto.drawImage(
+                sprites,
+                chao.spriteX, chao.spriteY,
+                chao.largura, chao.altura,
+                chao.x+chao.largura, chao.y,
+                chao.largura, chao.altura,
+            );
+
+        }
+};
+
+
+
 function loop(){
+    cidade.desenha();
+    chao.desenha();
     flappyBird.desenha();
 
     requestAnimationFrame(loop);
